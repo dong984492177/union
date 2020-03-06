4af73cc37014072bbfa9123b65a10739a8ac2b31
dong <984492177@qq.com>
2020-03-06 17:38:30 +0800

    001

--- src/main/java/com/dong/UnionApplication.java
+++ src/main/java/com/dong/UnionApplication.java
@@ -2,12 +2,9 @@ package com.dong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class UnionApplication  {
-
 
     public static void main(String[] args) {
         SpringApplication.run(UnionApplication.class, args);
